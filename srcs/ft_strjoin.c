c12b6f7ed699bebe2027046c54ab2f89e7a3ccc0
Fauna Polaris <max.maxpinheiro@gmail.com>
2023-05-09 16:28:09 -0300

    strjoin

--- srcs/ft_strjoin.c
+++ srcs/ft_strjoin.c
@@ -1,23 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fpolaris <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/26 09:24:02 by fpolaris          #+#    #+#             */
-/*   Updated: 2023/05/09 16:21:58 by fpolaris         ###   ########.fr       */
+/*   Created: 2023/05/09 13:12:49 by fpolaris          #+#    #+#             */
+/*   Updated: 2023/05/09 16:25:40 by fpolaris         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlen(const char *s)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	int	counter;
+	int		t_len;
+	char	*newstr;
+	int		counter;
+	int		i;
 
+	i = 0;
 	counter = 0;
-	while (s[counter])
+	t_len = ft_strlen(s1) + ft_strlen(s2) + 1;
+	newstr = malloc(t_len + 1);
+	if (newstr == 0)
+		return (NULL);
+	while (counter <= (int)ft_strlen(s1))
+	{
+		newstr[counter] = s1[counter];
 		counter++;
-	return (counter - 1);
+	}
+	while (counter <= t_len)
+	{
+		newstr[counter] = s2[i];
+		counter++;
+		i++;
+	}
+	newstr[counter] = '\0';
+	return (newstr);
 }
