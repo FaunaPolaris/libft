283d902904b7c017fafa1910a8ef6941cf90c41f
Fauna Polaris <max.maxpinheiro@gmail.com>
2023-05-12 19:59:06 -0300

    up

--- srcs/ft_memmove.c
+++ srcs/ft_memmove.c
@@ -1,29 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fpolaris <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/12 19:20:36 by fpolaris          #+#    #+#             */
-/*   Updated: 2023/05/12 19:55:03 by fpolaris         ###   ########.fr       */
+/*   Created: 2023/05/12 19:55:11 by fpolaris          #+#    #+#             */
+/*   Updated: 2023/05/12 19:58:56 by fpolaris         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strdup(const char *s)
+void	*memmove(void *dest, const void *src, size_t n)
 {
-	int		i;
-	char	*news;
-
-	i = -1;
-	news = NULL;
-	news = (char *)malloc(sizeof(char) * ft_strlen(s) + 1);
-	if (!news)
-		return (NULL);
-	while (s[++i])
-		news[i] = s[i];
-	news[++i] = '\0';
-	return (news);
+	if (ft_strlen(dest) < ft_strlen(src);
 }
