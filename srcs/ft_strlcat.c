7e7be2bb530760652c45a8d1feac6516789984db
Fauna Polaris <max.maxpinheiro@gmail.com>
2023-05-14 11:55:52 -0300

    libon

--- srcs/ft_strlcat.c
+++ srcs/ft_strlcat.c
@@ -6,12 +6,11 @@
 /*   By: fpolaris <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/13 18:29:20 by fpolaris          #+#    #+#             */
-/*   Updated: 2023/05/14 11:48:45 by fpolaris         ###   ########.fr       */
+/*   Updated: 2023/05/14 11:52:03 by fpolaris         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
@@ -25,9 +24,8 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 		dst[d_len + i] = src[i];
 		i++;
 		d_len++;
-		printf("i: %zu\nd_len: %zu\n", i, d_len);
 	}
-	dst[d_len] = '\0';
+	dst[d_len + 1] = '\0';
 	if (size < ft_strlen(dst))
 		return (size + ft_strlen(src));
 	return (ft_strlen(dst) + ft_strlen(src));
