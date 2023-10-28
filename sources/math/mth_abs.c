75a38762ae77a0fdd44ca92c2fae71bd86e2dd24
Fauna <max.maxpinheiro@gmail.com>
2023-10-28 20:22:11 -0300

    itoa_2.0

---
+++ sources/math/mth_abs.c
@@ -0,0 +1,8 @@
+#include "polarium.h"
+
+int	mth_abs(int nb)
+{
+	if (nb < 0)
+		return (-nb);
+	return (nb);
+}
---
+++ sources/types/int_len.c
@@ -0,0 +1,16 @@
+#include "polarium.h"
+
+int	int_len(int nb)
+{
+	int	output;
+
+	output = 0;
+	if (nb < 0)
+		output++;
+	while (mth_abs(nb) >= 10)
+	{
+		output++;
+		nb *= .1;
+	}
+	return (output);
+}
