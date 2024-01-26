1ecb1541cdc06930c69de5d76ee62dfefe7d38f7
Fauna <max.maxpinheiro@gmail.com>
2024-01-26 10:56:56 -0300

    parabola tweaks

--- sources/graphics/draw_parabola.c
+++ sources/graphics/draw_parabola.c
@@ -2,15 +2,16 @@
 
 int	draw_parabola(t_window *win, t_vector stt, int lenght)
 {
-	int		t = -1;
+	double		t = -lenght;
 	int		x;
 	int		y;
 
-	while (t++ < lenght)
+	while (t < lenght)
 	{
-			x = stt.x + t;
-			y = pow(stt.y + t, 2);
-			draw_pixel(win, x, y, stt.color);
+			x = (stt.x + t);
+			y = (pow(stt.y + t, 2)) * .1;
+			draw_pixel(win, x, y, WHITE);
+			t += PRECISION;
 	}
 	return (TRUE);
 }
