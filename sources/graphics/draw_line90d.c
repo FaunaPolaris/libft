5b5c01c2f6ca3dd46fdf21823820dc7a7a5765b8
Fauna <max.maxpinheiro@gmail.com>
2024-01-28 17:40:25 -0300

    line90d done, woth thickness

--- sources/graphics/draw_line90d.c
+++ sources/graphics/draw_line90d.c
@@ -1,10 +1,14 @@
 #include "libfpp.h"
 
+static void	st_apply_thickness_east_west(t_window *win, t_vector stt,
+				int i, int thickness);
+static void	st_apply_thickness_north_south(t_window *win, t_vector stt,
+				int i, int thickness);
+
 void	draw_line90d(t_window *win, t_vector stt, 
 				t_direction direction, int thickness)
 {
 	int	i;
-	int	j;
 
 	i = 0;
 	if (direction > 3)
@@ -16,21 +20,53 @@ void	draw_line90d(t_window *win, t_vector stt,
 	{
 		while (draw_pixel(win, stt.x, stt.y + i++, stt.color))
 		{
-			j = 0;
-			while (j++ < thickness)
-			{
-					draw_pixel(win, stt.x + j, stt.y + i, stt.color);
-					draw_pixel(win, stt.x - j, stt.y + i, stt.color);
-			}
+			st_apply_thickness_north_south(win, stt, i, thickness);
 		}
 	}
 	else if (direction == SOUTH)
-			while (draw_pixel(win, stt.x, stt.y + i--, stt.color))
-					;
+	{
+		while (draw_pixel(win, stt.x, stt.y + i--, stt.color))
+		{
+			st_apply_thickness_north_south(win, stt, i, thickness);
+		}
+	}
 	else if (direction == EAST)
-			while (draw_pixel(win, stt.x + i--, stt.y, stt.color))
-					;
+	{
+		while (draw_pixel(win, stt.x + i--, stt.y, stt.color))
+		{
+			st_apply_thickness_east_west(win, stt, i, thickness);
+		}
+	}
 	else if (direction == WEAST)
-			while (draw_pixel(win, stt.x + i++, stt.y, stt.color))
-					;
+	{
+		while (draw_pixel(win, stt.x + i++, stt.y, stt.color))
+		{
+			st_apply_thickness_east_west(win, stt, i, thickness);
+		}
+	}
+}
+static void	st_apply_thickness_north_south(t_window *win, t_vector stt,
+				int i, int thickness)
+{
+	int	j;
+
+	j = 0;
+	while (j++ < thickness)
+	{
+		draw_pixel(win, stt.x + j, stt.y + i, stt.color);
+		draw_pixel(win, stt.x - j, stt.y + i, stt.color);
+	}
+}
+
+static void	st_apply_thickness_east_west(t_window *win, t_vector stt,
+				int i, int thickness)
+{
+	int	j;
+
+	j = 0;
+	while (j++ < thickness)
+	{
+		draw_pixel(win, stt.x + i, stt.y + j, stt.color);
+		draw_pixel(win, stt.x + i, stt.y - j, stt.color);
+	}
 }
