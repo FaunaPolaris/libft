dd105c0555ca7fac8ddd5491f970bdaef5cf5a0b
Fauna <max.maxpinheiro@gmail.com>
2024-01-28 19:19:25 -0300

    cartesian table design 1.0

--- sources/graphics/draw_cartesian_table.c
+++ sources/graphics/draw_cartesian_table.c
@@ -7,8 +7,8 @@ void	draw_cartesian_table(t_window *win, int height, int width)
 {
 	st_draw_vertical_lines(win, height);
 	st_draw_horizontal_lines(win, width);
-	draw_line90d(win, (t_vector){height * .5, 0, 0, AZULAO}, WEAST, 2);
-	draw_line90d(win, (t_vector){0, width * .5, 0, AZULAO}, NORTH, 2);
+	draw_line90d(win, (t_vector){height * .5, 0, 0, CARMINE}, WEAST, 0);
+	draw_line90d(win, (t_vector){0, width * .5, 0, CARMINE}, NORTH, 0);
 	draw_update(win);
 }
 
@@ -19,14 +19,14 @@ static void	st_draw_vertical_lines(t_window *win, int height)
 	while (i < height * .5)
 	{
 		draw_line90d(win, (t_vector){(height * .5) + i, 0,
-						0, CARMINE}, WEAST, 0); 
+						0, AZULAO}, WEAST, 0); 
 		i += 25;
 	}
 	i = 0;
 	while (i < height * .5)
 	{
 		draw_line90d(win, (t_vector){(height * .5) - i, 0,
-						0, CARMINE}, WEAST, 0); 
+						0, AZULAO}, WEAST, 0); 
 		i += 25;
 	}
 }
@@ -38,14 +38,14 @@ static void	st_draw_horizontal_lines(t_window *win, int width)
 	while (i < width * .5)
 	{
 		draw_line90d(win, (t_vector){0, (width * .5) + i,
-						0, CARMINE}, NORTH, 0); 
+						0, AZULAO}, NORTH, 0); 
 		i += 25;
 	}
 	i = 0;
 	while (i < width * .5)
 	{
 		draw_line90d(win, (t_vector){0, (width * .5) - i,
-						0, CARMINE}, NORTH, 0); 
+						0, AZULAO}, NORTH, 0); 
 		i += 25;
 	}
 }
